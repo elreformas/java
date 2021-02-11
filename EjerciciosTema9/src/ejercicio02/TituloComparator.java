0270c7f949c040be89d6d9b049ec8caded7d6c14
elreformas <180046@iessierradeguara.com>
2021-02-11 17:44:40 +0100

    ejercicio 1 terminado y empezando ejercicio 2

---
+++ EjerciciosTema9/src/ejercicio02/TituloComparator.java
@@ -0,0 +1,10 @@
+package ejercicio02;
+import java.util.Comparator;
+
+class TituloComparator implements Comparator<Libro> {
+
+	public int compare(Libro l1, Libro l2) {
+		
+		return l1.dameTitulo().compareTo(l2.dameTitulo());
+	}
+}
