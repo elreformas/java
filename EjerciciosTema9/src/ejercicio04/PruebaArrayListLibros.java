e8707d2a50a5b25f3ebc6f55b05053aed6d66aab
elreformas <180046@iessierradeguara.com>
2021-02-11 23:06:47 +0100

    Ejercicio 4 completado

---
+++ EjerciciosTema9/src/ejercicio04/PruebaArrayListLibros.java
@@ -0,0 +1,41 @@
+package ejercicio04;
+
+import java.util.Collections;
+
+// PruebaListaLibros.java: prueba de la clase ArrayListLibros
+
+
+public class PruebaArrayListLibros {
+  public static void main(String args[]) {
+  	Persona p1 = new Persona(435762, 31, "Javier", "Hernandez Perez");
+    Fecha f1 = new Fecha(16,11,2001);
+    Libro l1 = new Libro("Introducción a Java",p1, 
+                         "0-13-031997-X",346,3,"Prentice-Hall","Madrid-España",f1);
+                         
+    Libro l2 = new Libro("Beginning Java",p1, 
+                         "0-13-031997-X",446,3,"Addison","Madrid-España",f1);                     
+    Libro l3 = new Libro("Avanzado Java",p1, 
+                         "0-13-031997-X",546,3,"McGraw-Hill","Madrid-España",f1);
+    
+                     
+    
+    ArrayListLibros a= new ArrayListLibros();
+    a.insertar(l1);
+    a.insertar(l2);
+    a.insertar(l3);	
+    
+    System.out.println(a.length());
+    
+    //a.eliminarLibro(l2);
+    
+    //System.out.println(a.buscaLibro(l3));
+    
+    //a.imprimir();
+    
+    //a.buscarContenido("Intro");
+    
+
+    
+  
+  }
+}
